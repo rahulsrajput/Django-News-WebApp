e11467bb7f9b4787fa3e14f8f480cd5a6f9e85f1
rahulsrajput <rahulsrajput29@gmail.com>
2024-03-23 14:52:58 +0530

    basic frontend template part, views, url basic done

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='home'),
+    path('', views.news_home, name='home'),
+    path('news_detail/', views.news_detail, name='detail'),
+    path('login/', views.login_page, name='login'),
+    path('signup/', views.signup_page, name='signup'),
+    path('logout/', views.logout_page, name='logout'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('addpost/', views.add_post, name='addpost'),
+    path('editpost/', views.edit_post, name='editpost'),
+
+    path('about/', views.about, name='about'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_page(request):
+    return render(request, 'login.html')
+
+def signup_page(request):
+    return render(request, 'signup.html')
+
+def logout_page(request):
+    pass
+
+
+
+
+
+def dashboard(request):
+    return render(request, 'dashboard/dashboard_page.html')
+
+def add_post(request):
+    return render(request, 'dashboard/add_post.html')
+
+def edit_post(request):
+    return render(request, 'dashboard/edit_post.html')
+
+
+
+def news_home(request):
+    return render(request, 'news_home.html')
+
+def news_detail(request):
+    return render(request, 'news_detail.html')
+
+def about(request):
+    return render(request, 'about.html')
