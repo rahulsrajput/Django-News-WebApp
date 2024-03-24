0bc3f8476457b00bdc7f1356fa277d6bf5b52d58
rahulsrajput <rahulsrajput29@gmail.com>
2024-03-24 19:06:17 +0530

    database changed to postgresql

--- news/settings.py
+++ news/settings.py
@@ -78,9 +78,18 @@ WSGI_APPLICATION = 'news.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'UjuOKGlweuxSQeYAClSdjbziNTGUFUMD',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '52154',
     }
 }
 
